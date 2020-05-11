9ed258ca1c9a3b5f3aa3846a3675568eb730a5bc
dung <896285621@qq.com>
2020-05-11 15:14:42 +0800

    common公共包

---
+++ ascend-common/src/main/java/com/dung/cn/AscendCommonApplication.java
@@ -0,0 +1,13 @@
+package com.dung.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AscendCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AscendCommonApplication.class, args);
+    }
+
+}
---
+++ ascend-common/src/test/java/com/dung/cn/AscendCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dung.cn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AscendCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
